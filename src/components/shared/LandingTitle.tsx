81bf9d5e2a3e95f461740f23b3d6510886a546f5
Mohamad <m.yousefi.isf@gmail.com>
2025-01-16 11:27:00 +0330

    0-0

--- src/components/shared/LandingTitle.tsx
+++ src/components/shared/LandingTitle.tsx
@@ -2,7 +2,7 @@ import staff from "./staff.svg";
 import staffB from "./staff-b.svg";
 // import Image from "next/image";
 import ExportedImage from "next-image-export-optimizer";
-import { ElProps } from "../../../types/types";
+import { ElProps } from "src/types/types";
 import { cva, VariantProps } from "class-variance-authority";
 
 interface LandingTitleProps extends ElProps, VariantProps<typeof landingTitle> {
